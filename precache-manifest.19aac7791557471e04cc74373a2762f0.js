df833db767173d0e3187d7c791fd9d064007dab9
RanaMuhammadIrshad <rmirshad@web.de>
2022-07-29 10:18:45 +0200

    Updates

---
+++ precache-manifest.19aac7791557471e04cc74373a2762f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc09835663abd1cde89adee95aa21f0",
+    "url": "/HTTP-Request-React-App/index.html"
+  },
+  {
+    "revision": "3e9a455c7050933568b8",
+    "url": "/HTTP-Request-React-App/static/css/main.d422f95d.chunk.css"
+  },
+  {
+    "revision": "3272caa3b0f87e1ae8a4",
+    "url": "/HTTP-Request-React-App/static/js/2.a1330267.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/HTTP-Request-React-App/static/js/2.a1330267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9a455c7050933568b8",
+    "url": "/HTTP-Request-React-App/static/js/main.574be0b4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/HTTP-Request-React-App/static/js/main.574be0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6097d40ce49186c7d6",
+    "url": "/HTTP-Request-React-App/static/js/runtime-main.e7c1d76c.js"
+  }
+]);
